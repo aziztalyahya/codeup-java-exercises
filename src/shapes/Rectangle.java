51c99b3e4deedfd3f20786e10885969cd88a07ce
aziztalyahya <aziz.t.alyahya@gmail.com>
2019-01-16 15:53:00 -0600

    finished inheritance and polymorphism examples

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,20 +1,25 @@
 package shapes;
 
 public class Rectangle {
-    private int length;
-    private int width;
+    private double length;
+    private double width;
 
-    public Rectangle(int length, int width){
+    public Rectangle(){
         this.length = length;
         this.width = width;
     }
 
-    public int getArea(){
-        int area = this.length *  this.width;
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea(){
+        double area = this.length *  this.width;
         return area;
     }
-    public int getPerimeter(){
-        int perimeter = (2 * this.length) + (2 * this.width);
+    public double getPerimeter(){
+        double perimeter = (2 * this.length) + (2 * this.width);
         return perimeter;
     }
 }
